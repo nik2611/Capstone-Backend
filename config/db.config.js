79513a9bcbb593dbb6d1182acce3706e02e8dbfc
nikhilgangeri <nikhilgangeri@gmail.com>
2022-10-01 21:41:50 +0530

    added code to dbConfig to initialise the role collection with learner and educator roles if it is empty

--- config/db.config.js
+++ config/db.config.js
@@ -1,49 +1,54 @@
 const db = require("./models");
 
 module.exports = async function expmongoose() {
-  await db.mongoose.connect(process.env.CONN)
+  await db.mongoose
+    .connect(process.env.CONN)
     .then(() => {
-        console.log("Successfully connected to MongoDB.");
-        //initial();
+      console.log("Successfully connected to MongoDB.");
+      //initial();
     })
     .catch((error) => {
-        console.log("Connection error: ", error);
-        process.exit()
+      console.log("Connection error: ", error);
+      process.exit();
     });
 };
 
-// function initial() {
-//     Role.estimatedDocumentCount((err, count) => {
-//       if (!err && count === 0) {
-//         new Role({
-//           name: "user"
-//         }).save(err => {
-//           if (err) {
-//             console.log("error", err);
-//           }
-  
-//           console.log("added 'user' to roles collection");
-//         });
-  
-//         new Role({
-//           name: "moderator"
-//         }).save(err => {
-//           if (err) {
-//             console.log("error", err);
-//           }
-  
-//           console.log("added 'moderator' to roles collection");
-//         });
-  
-//         new Role({
-//           name: "admin"
-//         }).save(err => {
-//           if (err) {
-//             console.log("error", err);
-//           }
-  
-//           console.log("added 'admin' to roles collection");
-//         });
-//       }
-//     });
-//   }
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "learner",
+      })
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(201).json({
+            message: "learner role created",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        });
+
+      new Role({
+        name: "educator",
+      })
+      .save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: "educator role created"
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+    }
+  });
+}
