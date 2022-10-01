ae81019e43b1813b7195bebd80ef40f2f89504ec
nikhilgangeri <nikhilgangeri@gmail.com>
2022-10-01 20:29:11 +0530

    created user and role model in models directory

---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: { type: String, required: true},
+    email: { type: String, required: true, unique: true, 
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
+    password: { type: String, required: true},
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
+  })
+);
+
+module.exports = User;
