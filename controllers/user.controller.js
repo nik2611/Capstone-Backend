e84781d7127b3d82a7dd97523e40d36bd2a5acb0
nikhilgangeri <nikhilgangeri@gmail.com>
2022-10-05 07:20:56 +0530

    installed multer multerS3 and awsSdk packages for image and video uploads and initialised awsS3 with required keys

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,8 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+
 
 exports.allAccess = (req, res) => {
   res.status(200).send("Public Content.");
@@ -7,6 +12,18 @@ exports.allAccess = (req, res) => {
     res.status(200).send("Learner Content.");
   };
   
-  exports.educatorBoard = (req, res) => {
+
+  //Educator Board controllers
+  exports.educatorBoardDemoVideo = (req, res) => {
+
     res.status(200).send("Educator Content.");
+
+    const s3 = new aws.S3({
+      accessKeyId: process.env.S3-ACCESS-KEY,
+      secretAccessKey: process.env.S3-SECRET-ACCESS-KEY,
+      region: process.env.S3-BUCKET-REGION,
+    });
+
+
+
   };
