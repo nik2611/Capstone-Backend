d1aa2ff85aa0358ab3704b1d6c7e058e656253c6
nikhilgangeri <nikhilgangeri@gmail.com>
2022-10-14 01:16:59 +0530

    made courseRegister route to get method

--- routes/learner.routes.js
+++ routes/learner.routes.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
     controller.learnerBoardDetailedCourseInfo
   );
 
-  app.post(
+  app.get(
     "/api/learner/RegisterCourse/:courseID",
     [authJwt.verifyToken, authJwt.isLearner],
     controller.learnerBoardRegisterCourse
